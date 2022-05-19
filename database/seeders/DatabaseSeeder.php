234dd28b06d093a49287a967730ab2889af92a2f
AlyHammoud <hammoud1.aly1@gmail.com>
2022-05-20 01:18:40 +0300

    seed with factory and faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
         // check factoroy folder for user factory
         //sail artisan migrate:refresh --seed // this will empty all records in db then run seed
 
+        listing::factory(6)->create();
+
         \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
@@ -26,27 +28,27 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        listing::create([
-            'title' => 'title 1',
-            'tags' => 'tags 1',
-            'company' => 'company 1',
-            'location' => 'location 1',
-            'email' => 'email 1',
-            'website' => 'website 1',
-            'description' => 'description 1description 1description 1description 1
-            description 1description 1description 1description 1',
-        ]);
-
-        listing::create([
-            'title' => 'title 2',
-            'tags' => 'tags 2',
-            'company' => 'company 2',
-            'location' => 'location 2',
-            'email' => 'email 2',
-            'website' => 'website 2',
-            'description' => 'description 2description 1description 1description 1
-            description 1description 1description 1description 1',
-        ]);
+//        listing::create([
+//            'title' => 'title 1',
+//            'tags' => 'tags 1',
+//            'company' => 'company 1',
+//            'location' => 'location 1',
+//            'email' => 'email 1',
+//            'website' => 'website 1',
+//            'description' => 'description 1description 1description 1description 1
+//            description 1description 1description 1description 1',
+//        ]);
+//
+//        listing::create([
+//            'title' => 'title 2',
+//            'tags' => 'tags 2',
+//            'company' => 'company 2',
+//            'location' => 'location 2',
+//            'email' => 'email 2',
+//            'website' => 'website 2',
+//            'description' => 'description 2description 1description 1description 1
+//            description 1description 1description 1description 1',
+//        ]);
 
     }
 }
