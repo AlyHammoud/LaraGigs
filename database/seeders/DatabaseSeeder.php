7382c15d9adcbd98cc82778f0d992a64b2e6b920
AlyHammoud <hammoud1.aly1@gmail.com>
2022-05-20 01:09:43 +0300

    seed manually

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\listing;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,12 +17,36 @@ class DatabaseSeeder extends Seeder
     {
         // run sail artisan db:seed
         // check factoroy folder for user factory
+        //sail artisan migrate:refresh --seed // this will empty all records in db then run seed
 
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        listing::create([
+            'title' => 'title 1',
+            'tags' => 'tags 1',
+            'company' => 'company 1',
+            'location' => 'location 1',
+            'email' => 'email 1',
+            'website' => 'website 1',
+            'description' => 'description 1description 1description 1description 1
+            description 1description 1description 1description 1',
+        ]);
+
+        listing::create([
+            'title' => 'title 2',
+            'tags' => 'tags 2',
+            'company' => 'company 2',
+            'location' => 'location 2',
+            'email' => 'email 2',
+            'website' => 'website 2',
+            'description' => 'description 2description 1description 1description 1
+            description 1description 1description 1description 1',
+        ]);
+
     }
 }
